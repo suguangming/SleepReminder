7c47e687f7a0dc134e181d05c836c239bb30e799
ming <suguangming95@gmail.com>
2019-10-31 23:20:18 +0800

    notification added.

--- app/src/main/java/com/ming/sleepreminder/MainActivity.java
+++ app/src/main/java/com/ming/sleepreminder/MainActivity.java
@@ -1,12 +1,18 @@
 package com.ming.sleepreminder;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import java.text.DecimalFormat;
 import java.util.Calendar;
+import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         //get time
-        Calendar calendar = Calendar.getInstance();
+        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("GMT+8"));
         int year = calendar.get(Calendar.YEAR);
         int month = calendar.get(Calendar.MONTH)+1;
         int day = calendar.get(Calendar.DAY_OF_MONTH);
@@ -55,15 +61,38 @@ public class MainActivity extends AppCompatActivity {
 
         int hoursLeft = 0;
         int minutesLeft = 0;
+        double oneCycle = 90.0;
+        double cyclesLeft = 0;
         if (hour > 7 ){
             hoursLeft = 24-hour+7-1;
         } else {
             hoursLeft = 7-hour-1;
         }
         minutesLeft = hoursLeft * 60 + 60 - minute;
-        double oneCycle = 90.0;
-        double cyclesLeft =minutesLeft/oneCycle;
+        cyclesLeft =minutesLeft/oneCycle;
+//        hoursLeftTextView.setText(String.format("%s",hoursLeft));
+//        cyclesLeftTextView.setText((new DecimalFormat("0.00")).format(cyclesLeft));
 
+        //Notification
+        NotificationManager manager;
+        NotificationChannel channel;
+        String CHANNEL_ID = "default";
+        String CHANNEL_NAME = "Default Channel";
+        String CHANNEL_DESCRIPTION = "this is default channel!";
+        manager=(NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH);
+        channel.setDescription(CHANNEL_DESCRIPTION);
+        manager.createNotificationChannel(channel);
+        Notification notification =new NotificationCompat.Builder(this,"default")
+                .setContentTitle(String.format("%s",hoursLeft)+" hours left.")
+                .setContentText((new DecimalFormat("0.0")).format(cyclesLeft) + " cycles left.")
+                .setWhen(System.currentTimeMillis())
+                .setAutoCancel(true)
+                .setSmallIcon(R.drawable.small_bed_icon)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.drawable.bed_icon))
+                .setTimeoutAfter(15000)
+                .build();
+        manager.notify(1,notification);
         hoursLeftTextView.setText(String.format("%s",hoursLeft));
         cyclesLeftTextView.setText((new DecimalFormat("0.00")).format(cyclesLeft));
     }
