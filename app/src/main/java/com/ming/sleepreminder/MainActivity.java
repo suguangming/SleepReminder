7736ec62a81f119f2c41b9828032ece52a4f53c4
minming <suguangming95@gmail.com>
2019-10-31 15:50:06 +0800

    basic function finished.

--- app/src/main/java/com/ming/sleepreminder/MainActivity.java
+++ app/src/main/java/com/ming/sleepreminder/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
@@ -22,12 +24,29 @@ public class MainActivity extends AppCompatActivity {
         TextView timeTextView2 = findViewById(R.id.timeTextView2);
         TextView timeTextView3 = findViewById(R.id.timeTextView3);
 
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy年MM月dd日   HH:mm");
-        formatter.setTimeZone(TimeZone.getTimeZone("GMT+8"));
-        Date nowTime = new Date(System.currentTimeMillis());
-        String nowTimeStr = formatter.format(nowTime);
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+        int second = calendar.get(Calendar.SECOND);
+        String nowTimeStr = year+"年"+month+"月"+day+"日"+hour+":"+minute;
         timeTextView1.setText(nowTimeStr);
 
+        int hoursLeft = 0;
+        int minutesLeft = 0;
+        if (hour > 7 ){
+            hoursLeft = 24-hour+7-1;
+        } else {
+            hoursLeft = 7-hour-1;
+        }
+        minutesLeft = hoursLeft * 60 + 60 - minute;
+        double oneCycle = 90.0;
+        double cyclesLeft =minutesLeft/oneCycle;
 
+        timeTextView2.setText(String.format("%s",hoursLeft));
+        timeTextView3.setText((new DecimalFormat("0.00")).format(cyclesLeft));
     }
 }
